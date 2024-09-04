9e9ff2c5f3c17be2ec2651f881c4fde0471cf645
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-09-04 18:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstWord(s) {
+function firstWord(s) :
   // your code here
-}
+	if not s:
+	return ''
+	
+	 s=s.strip()
+	space_index=s.find(' ')
 
+	if space_index=-1:
+	return s;
+	  
+
+return s[:space_index]
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));   
